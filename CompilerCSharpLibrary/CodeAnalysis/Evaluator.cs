59c178892fce99143c6149081ef87ae61728dffd
aylin <ajlinkadan3224@gmail.com>
2024-02-18 19:23:56 +0300

    Упрощены методы класса Evaluator

--- CompilerCSharpLibrary/CodeAnalysis/Evaluator.cs
+++ CompilerCSharpLibrary/CodeAnalysis/Evaluator.cs
@@ -22,70 +22,96 @@ namespace CompilerCSharpLibrary.CodeAnalysis
         //Вычисляет выражение, используя построенное АСД
         private object EvaluateExpression(BoundExpression node)
         {
-            if (node is BoundLiteralExpression n){
-                return n.Value;
-            }
-            /*
-            Если node типа BoundVariableExpression,
-            то возвращает значение переменной из списка всех переменных
-            */
-            if (node is BoundVariableExpression v){
-                return _variables[v.Variable];
-            }
-            /*
-            Если происходит приравнивание переменной какого-то
-            выражения, то получает результат выражения, приравнивает
-            этот результат к переменной и возвращает результат выражения
-            */
-            if (node is BoundAssignmentExpression a){
-                var value = EvaluateExpression(a.Expression);
-                _variables[a.Variable] = value;
-                return value;
-            }
-            if (node is BoundUnaryExpression u){
-                object operand = EvaluateExpression(u.Operand);
-
-                switch (u.Op.Kind){
-                    case BoundUnaryOperatorKind.Identity:
-                        return (int)operand;
-                    case BoundUnaryOperatorKind.Negation:
-                        return -(int)operand;
-                    case BoundUnaryOperatorKind.LogicalNegation:
-                        return !(bool)operand;
-                    
-                    default:
-                        throw new Exception($"Unexpected unary operator {u.Op.Kind}");
-                }
-            }
-            if (node is BoundBinaryExpression b){
-                object left = EvaluateExpression(b.Left);
-                object right = EvaluateExpression(b.Right);
-
-                switch (b.Op.Kind){
-                    case BoundBinaryOperatorKind.Addition:
-                        return (int)left + (int)right;
-                    case BoundBinaryOperatorKind.Substraction:
-                        return (int)left - (int)right;
-                    case BoundBinaryOperatorKind.Multiplication:
-                        return (int)left * (int)right;
-                    case BoundBinaryOperatorKind.Division:
-                        return (int)left / (int)right;
-
-                    case BoundBinaryOperatorKind.LogicalAnd:
-                        return (bool)left && (bool)right;
-                    case BoundBinaryOperatorKind.LogicalOr:
-                        return (bool)left || (bool)right;
-                    case BoundBinaryOperatorKind.Equals:
-                        return Equals(left, right);
-                    case BoundBinaryOperatorKind.NotEquals:
-                        return !Equals(left, right);
-                    
-                    default:
-                        throw new Exception($"Unexpected binary operator {b.Op.Kind}");
-                }
+            switch (node.Kind)
+            {
+                case BoundNodeKind.LiteralExpression:
+                    return EvaluateLiteralExpression((BoundLiteralExpression)node);
+                case BoundNodeKind.VariableExpression:
+                    return EvaluateVariableExpression((BoundVariableExpression)node);
+                case BoundNodeKind.AssignmentExpression:
+                    return EvaluateAssignmentExpression((BoundAssignmentExpression)node);
+                case BoundNodeKind.UnaryExpression:
+                    return EvaluateUnaryExpression((BoundUnaryExpression)node);
+                case BoundNodeKind.BinaryExpression:
+                    return EvaluateBinaryExpression((BoundBinaryExpression)node);
+                default:
+                    throw new Exception($"Unexpected node {node.Kind}");
             }
 
-            throw new Exception($"Unexpected node {node.Kind}");
+        }
+
+        private object EvaluateBinaryExpression(BoundBinaryExpression b)
+        {
+            object left = EvaluateExpression(b.Left);
+            object right = EvaluateExpression(b.Right);
+
+            switch (b.Op.Kind)
+            {
+                case BoundBinaryOperatorKind.Addition:
+                    return (int)left + (int)right;
+                case BoundBinaryOperatorKind.Substraction:
+                    return (int)left - (int)right;
+                case BoundBinaryOperatorKind.Multiplication:
+                    return (int)left * (int)right;
+                case BoundBinaryOperatorKind.Division:
+                    return (int)left / (int)right;
+
+                case BoundBinaryOperatorKind.LogicalAnd:
+                    return (bool)left && (bool)right;
+                case BoundBinaryOperatorKind.LogicalOr:
+                    return (bool)left || (bool)right;
+                case BoundBinaryOperatorKind.Equals:
+                    return Equals(left, right);
+                case BoundBinaryOperatorKind.NotEquals:
+                    return !Equals(left, right);
+
+                default:
+                    throw new Exception($"Unexpected binary operator {b.Op.Kind}");
+            }
+        }
+
+        private object EvaluateUnaryExpression(BoundUnaryExpression u)
+        {
+            object operand = EvaluateExpression(u.Operand);
+
+            switch (u.Op.Kind)
+            {
+                case BoundUnaryOperatorKind.Identity:
+                    return (int)operand;
+                case BoundUnaryOperatorKind.Negation:
+                    return -(int)operand;
+                case BoundUnaryOperatorKind.LogicalNegation:
+                    return !(bool)operand;
+
+                default:
+                    throw new Exception($"Unexpected unary operator {u.Op.Kind}");
+            }
+        }
+
+        /*
+        Если происходит приравнивание переменной какого-то
+        выражения, то получает результат выражения, приравнивает
+        этот результат к переменной и возвращает результат выражения
+        */
+        private object EvaluateAssignmentExpression(BoundAssignmentExpression a)
+        {
+            var value = EvaluateExpression(a.Expression);
+            _variables[a.Variable] = value;
+            return value;
+        }
+
+        /*
+        Если node типа BoundVariableExpression,
+        то возвращает значение переменной из списка всех переменных
+        */
+        private object EvaluateVariableExpression(BoundVariableExpression v)
+        {
+            return _variables[v.Variable];
+        }
+
+        private static object EvaluateLiteralExpression(BoundLiteralExpression n)
+        {
+            return n.Value;
         }
     }
 }
