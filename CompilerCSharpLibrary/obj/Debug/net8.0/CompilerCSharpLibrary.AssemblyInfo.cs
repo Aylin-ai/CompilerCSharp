8ce867f0e818c655d51244001f8cc39e8cafb45b
aylin <ajlinkadan3224@gmail.com>
2024-02-27 16:57:37 +0300

    Esc удаляет весь код при нажатии

--- CompilerCSharpLibrary/obj/Debug/net8.0/CompilerCSharpLibrary.AssemblyInfo.cs
+++ CompilerCSharpLibrary/obj/Debug/net8.0/CompilerCSharpLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CompilerCSharpLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7419ea289717899798690d10cfe84938502a45dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f289614b5110c3f9ea54a28cbb5dd984cb06fe6b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CompilerCSharpLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CompilerCSharpLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- InterpreterCSharp/Repl.cs
+++ InterpreterCSharp/Repl.cs
@@ -210,7 +210,9 @@ namespace InterpreterCSharp
         private void HandleEscape(ObservableCollection<string> document, SubmissionView view)
         {
             document[view.CurrentLine] = string.Empty;
-            view.CurrentCharacter = 0;
+            document.Clear();
+            document.Add(string.Empty);
+            view.CurrentLine = 0;
         }
 
         private void HandleEnter(ObservableCollection<string> document, SubmissionView view)
